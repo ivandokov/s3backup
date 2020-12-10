2ba8d3e2d829c9aba0ba0b30c3964de11343290d
Ivan Dokov <ivan@jetspark.io>
2020-12-10 20:48:48 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# s3backup
-Script to backup any folder to S3 compatible storage
+# S3 Backup
+Script to backup any folder to S3 compatible storage using [s3cmd](https://github.com/s3tools/s3cmd) which can be installed with your default software manager
+```bash
+sudo apt install s3cmd
+```
+Requires Bash 4.2 or later.
+
+# Usage
+```bash
+s3backup.sh [DIRECTORY TO BACKUP] [BACKUP AT S3 DIRECTORY] [SLACK WEBHOOK] 
+```
+
+You can use this script in cron jobs to regularly backup. For example every 6h.
+```bash
+0 */6 * * * /path/to/s3backup.sh /var/www/html s3/backup/directory https://hooks.slack.com/services/A/B/C >> /dev/null 2>&1
+```
---
+++ s3backup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+BACKUP_FROM=$1
+BACKUP_TO=$2
+SLACK_WEBHOOK=$3
+
+if [ -z "${BACKUP_FROM}" ] || [ ! -d "${BACKUP_FROM}" ]; then
+    echo "Backup directory not found"
+    exit 1
+fi
+
+if [ -z "${BACKUP_TO}" ]; then
+    echo "Backup to directory is required"
+    exit 1
+fi
+
+[[ "${BACKUP_FROM}" != */ ]] && BACKUP_FROM="${BACKUP_FROM}/"
+[[ "${BACKUP_TO}" != */ ]] && BACKUP_TO="${BACKUP_TO}/"
+[[ "${BACKUP_TO}" != s3://* ]] && BACKUP_TO="s3://${BACKUP_TO}"
+
+BACKUP=`s3cmd --quiet sync ${BACKUP_FROM} ${BACKUP_TO}`
+
+# Backup process errored
+if [ $? -ne 0 ]; then
+    # If Slack webhook is not provided simply show the backup output
+    if [[ -z "$SLACK_WEBHOOK" ]]; then
+        echo "${BACKUP}"
+    # Otherwise send Slack notification
+    else
+        # Escape Slack special characters
+        BACKUP=${BACKUP//&/&amp;}
+        BACKUP=${BACKUP//</&lt;}
+        BACKUP=${BACKUP//>/&gt;}
+
+        # Prefix the output with a quote symbol
+        BACKUP=`echo "${BACKUP}" | sed -e 's/^/>/'`
+
+        curl -s -X POST \
+            -H 'Content-type: application/json' \
+            --data "{\"type\":\"mrkdwn\",\"text\":\"*Couldn't sync to backup storage*\n\n\$BACKUP\"}" \
+            $SLACK_WEBHOOK  > /dev/null
+    fi
+fi
